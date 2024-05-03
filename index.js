e2dfbfaa78de2fc1297ccc4579bf30ba4d7ec57a
Kaleb Fox <kalebfox7@yahoo.com>
2024-05-03 01:03:46 -0400

    line 4 mostly and go over 12-14

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(thankYou, eventN) {
+    const emptyA = []
+    for(let i = 0; i < thankYou.length; i++){
+        console.log(`Thank you, ${thankYou[i]}, for the wonderful ${eventN} gift!`)
+        emptyA.push(`Thank you, ${thankYou[i]}, for the wonderful ${eventN} gift!`)
+    }
+        return emptyA
+
+}
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number--)
+    }
+}
